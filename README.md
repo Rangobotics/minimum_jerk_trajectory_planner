04507ed0fbfc9d413d323a96788d57d5b657106a
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-04-19 01:10:57 +0200

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# minimum_jerk_trajectory_planner
-A minimum jerk trajectory generation basic script.
+# Trajectory Planning
+
+A minimum jerk trajectory generation basic script right now, planning on adding more trajectory generators.
+
+## Simulation
+
+![anim](res/minimum_jerk.gif)
+
+## Trajectory (one plot per Pose elements (x, y, theta))
+
+![im1](res/min_jerk_traj1.png)
+![im2](res/min_jerk_traj2.png)
+![im3](res/min_jerk_traj3.png)
--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def main():
     pose_start_2 = Pose(1, 5, 0)
     pose_start_3 = Pose(18, 3, 1.57)
 
-    robot_1 = Robot("Yellow Robot", "y", 0.5, controller, pose_start_1, pose_target1)
+    robot_1 = Robot("Yellow Robot", "y", 0.6, controller, pose_start_1, pose_target1)
     robot_2 = Robot("Black Robot", "k", 1.0, controller, pose_start_2, pose_target2)
-    robot_3 = Robot("Blue Robot", "b", 3.0, controller, pose_start_3, pose_target3)
+    robot_3 = Robot("Blue Robot", "b", 1.5, controller, pose_start_3, pose_target3)
 
     robots: list[Robot] = [robot_1, robot_2, robot_3]
 
